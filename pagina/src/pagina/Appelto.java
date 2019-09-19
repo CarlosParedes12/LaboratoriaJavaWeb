ee3b0a1d68dbf3142e80e1dd6289fb2b05b2d678
CarlosParedes12 <53574990+CarlosParedes12@users.noreply.github.com>
2019-09-19 09:59:23 -0600

    Add files via upload

---
+++ pagina/src/pagina/Appelto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pagina;
+
+import java.applet.Applet;
+import 
+
+/**
+ *
+ * @author alumno
+ */
+public class Appelto extends Applet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded into
+     * the browser.
+     */
+    public void init() {
+        // TODO start asynchronous download of heavy resources
+    }
+
+    // TODO overwrite start(), stop() and destroy() methods
+    public void Graphics (g){
+        
+    }
+}
